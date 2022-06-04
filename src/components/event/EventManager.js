5ad2d22f83a9edf431d026d936508de0885ce72e
michellecaner <michellecaner@gmail.com>
2022-06-04 13:07:43 -0500

    game & event lists rendering

---
+++ src/components/event/EventManager.js
@@ -0,0 +1,10 @@
+const remoteURL = "http://localhost:8000"
+
+export const getEvents = () => {
+  return fetch("http://localhost:8000/events", {
+    headers:{
+        "Authorization": `Token ${localStorage.getItem("lu_token")}`
+    }
+})
+    .then(response => response.json())
+}
---
+++ src/components/game/GameManager.js
@@ -0,0 +1,10 @@
+const remoteURL = "http://localhost:8000"
+
+export const getGames = () => {
+  return fetch("http://localhost:8000/games", {
+      headers:{
+          "Authorization": `Token ${localStorage.getItem("lu_token")}`
+      }
+  })
+      .then(response => response.json())
+}
